49c55c9b6d9914f2cfe37e3f3eb83ef53a82521d
Serhii Romanov <serhii_romanov@epam.com>
2021-08-19 16:13:45 +0300

    [EDP-DDM-8496] logging configs

---
+++ src/main/java/com/epam/digital/data/platform/starter/logger/LoggingAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.epam.digital.data.platform.starter.logger;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+// TODO do we need this one?
+public class LoggingAutoConfiguration {
+
+}
---
+++ src/test/java/com/epam/digital/data/platform/starter/logger/aspect/NoopTest.java
@@ -0,0 +1,12 @@
+package com.epam.digital.data.platform.starter.logger.aspect;
+
+import org.junit.jupiter.api.Test;
+
+class NoopTest {
+
+  @Test
+  public void noop() {
+    //noop
+  }
+
+}
